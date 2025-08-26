a486aef0fcb2ef159e7ba55105d5a48a80b55eb7
IIDonaji <imedrano@ucsb.edu>
2025-08-25 20:23:26 -0700

    tidy data

---
+++ scratch/Making-Spaghetti.R
@@ -0,0 +1,43 @@
+library(here)
+library(tidyverse)
+library(janitor)
+library(skimr)
+library(lubridate)
+
+Q1 <- read_csv(here("data", "QuebradaCuenca1-Bisley.csv" ))
+names(Q1)
+class(Q1$Sample_Date)
+
+Q2 <- read_csv(here("data", "QuebradaCuenca2-Bisley.csv" ))
+names(Q2)
+
+Q3 <- read_csv(here("data", "QuebradaCuenca3-Bisley.csv"))
+names(Q3)
+
+PRM <- read_csv(here("data", "RioMameyesPuenteRoto.csv"))
+names(PRM)
+
+All_df <- PRM %>% 
+  mutate(Sample_Date = ymd(Sample_Date)) %>% 
+  full_join(Q1) %>% 
+  full_join(Q2) %>% 
+  full_join(Q3) %>% 
+  clean_names()
+names(All_df)
+
+skim(All_df)
+
+df_long <- All_df %>% 
+  pivot_longer(cols = c(k, no3_n, mg, ca, nh4_n),
+               names_to = "ions",
+               values_to = "water_concen") 
+  
+small_df <- df_long %>% 
+  select(sample_id, sample_date, ions, water_concen) %>% 
+  rename(site = sample_id ,
+         date = sample_date)
+
+
+  
+  
+  
