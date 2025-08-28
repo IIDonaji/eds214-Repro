4546966b0267f792eb4f259c1d945d892b4bf2ab
IIDonaji <imedrano@ucsb.edu>
2025-08-27 21:01:42 -0700

    this commit addresses the issue I created earlier See #1

---
+++ R/moving_average_FUN.R
@@ -0,0 +1,10 @@
+# Function
+# add disciptive details
+moving_average <- function(focal_date, dates, conc, win_size_wks) {
+  is_in_window <- (dates > focal_date - (win_size_wks / 2) * 7) &
+    (dates < focal_date + (win_size_wks /2) *7)
+  conc_window <- conc[is_in_window]
+  result <- mean(conc_window, na.rm = TRUE)
+  
+  return(result)
+}
