cc25bc219aad7c6fe0ffc0f6ba6debcc2f0ed713
IIDonaji <imedrano@ucsb.edu>
2025-08-29 14:34:57 -0700

    environment initialization script done

---
+++ 0_init_envi.R
@@ -0,0 +1,11 @@
+################################################################################
+##                          Environment Initialization                         ##
+################################################################################
+
+# Package Installations used to run this analysis 
+intall.packages("here")
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("skimr")
+install.packages("lubridate")
+install.packages("dplyr")
